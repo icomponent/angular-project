6681dffc64987aaeda6334cff81a03e2f20f3139
zhangxishuo <zhangxishuo1998@outlook.com>
2020-02-10 11:05:45 +0800

    auth-routing

--- projects/sso/src/app/auth/auth.module.ts
+++ projects/sso/src/app/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
