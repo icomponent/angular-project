1f4f577daf0b3de457d28b0920cc80726cd7e48b
zhangxishuo <zhangxishuo1998@outlook.com>
2020-02-10 12:40:53 +0800

    transfer

--- projects/common/src/public-api.ts
+++ projects/common/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/common.service';
 export * from './lib/common.component';
 export * from './lib/common.module';
+
+export * from './lib/transfer/auth/auth-token';
+export * from './lib/transfer/auth/common-credentials';
