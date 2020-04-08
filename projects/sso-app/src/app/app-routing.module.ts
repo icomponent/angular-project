b2722ee009701b5e57684ab35d38af0b4a836218
张喜硕 <zhangxishuo1998@outlook.com>
2020-04-08 09:15:51 +0800

    layout

--- projects/sso-app/src/app/app-routing.module.ts
+++ projects/sso-app/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
